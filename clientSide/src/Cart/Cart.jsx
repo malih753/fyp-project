ce37744907bdb16d4f1c77d88bc79b909a3ff9c3
malih753 <malihawaseem998@gmail.com>
2024-05-06 10:20:36 +0100

    add the new user data in and issue in pdf file

--- clientSide/src/Cart/Cart.jsx
+++ clientSide/src/Cart/Cart.jsx
@@ -14,9 +14,21 @@ import { MDBListGroup, MDBListGroupItem } from 'mdb-react-ui-kit';
 import axios from 'axios';
 import img from './/../assets/1.png'
 import './cart.css';
+import DownloadPDFButton from '../pages/DownloadPdfBtn';
 
 const Cart = () => {
-<<<<<<< HEAD
+  const [selectedOption, setSelectedOptions] = useState('');
+  const [gender, setGender] = useState('');
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const [zip, setZip] = useState('');
+  const [phone_no, setPhone] = useState('');
+  const [state, setState] = useState('');
+  const [country, setCountry] = useState('');
+  const [email, setEmail] = useState('');
+  const [age, setAge] = useState('');
+  const [street, setStreet] = useState('');
+  const [city, setCity] = useState('');
   const [formData, setFormData] = useState({
     name: '',
     age: '',
@@ -30,7 +42,7 @@ const Cart = () => {
     country: '',
     email: ''
   });
-  
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
@@ -41,34 +53,6 @@ const Cart = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-=======
- 
-  // const [selectedOption, setSelectedOption] = useState('');
-  const [age, setAge] = useState('')
-  const [gender, setGender] = useState('')
-  const [genderOptions, setGenderOptions] = useState(["male", "female", "other"])
-  const [name, setName] = useState('')
-  const [phone, setPhone] = useState('')
-  // const [address, setAddress] = useState('')
-  const [email, setEmail] = useState('')
-  const [state, setState] = useState('')
-  const [city, setCity] = useState('')
-  const [country, setCountry] = useState('')
-  const [street, setStreet] = useState('')
-  const [zip, setZip] = useState('')
- 
-
-  const handleGender =(e)=>{
-    e.preventDefault()
-    setGenderOptions(e.target.value)
-  }
- 
-
-
-  const handleSubmit = async (e,req,res) => {
-    e.preventDefault(); // Prevent default form submission behavior
-    console.log('info data');
->>>>>>> 85161aef2b8f7b2cd67111249775ed7cf03e8915
     try {
       const response = await axios.post('http://localhost:4000/api/v1/info', formData);
       console.log(response.data);
@@ -78,18 +62,11 @@ const Cart = () => {
       alert("Error submitting form. Please try again.");
     }
   };
-<<<<<<< HEAD
 
-  // const handleGender = (e) => {
-  //   e.preventDefault()
-  //   setGender(e.target.value)
-  // }
-=======
-  const handleOptionClick = (option) => {
-    setSelectedOption(option);
-  };
- 
->>>>>>> 85161aef2b8f7b2cd67111249775ed7cf03e8915
+  const handleGender = (e) => {
+    e.preventDefault()
+    setGender(e.target.value)
+  }
   return (
     <div>
       <ProfileHeader />
@@ -146,148 +123,37 @@ const Cart = () => {
               <p style={{ paddingLeft: "60px", paddingBottom: "20px" }}> Lorem ipsum dolor dolor sit amet dolor sit amet.</p>
             </div>
           </div>
+          <DownloadPDFButton/>
         </div>
         <div className="right-div">
           <div className='booking-details'>
             <h5>Booking Details</h5>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, ab.</p>
           </div>
-<<<<<<< HEAD
-          <form className='form' onSubmit={handleSubmit}>
-            <div className='name'>
-              <h5>Name</h5>
-              <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter Name" />
-              <h5>Gender</h5>
-              
-              <select id="gender" value={gender} onChange={handleGender}>
-        <option value="">Select gender</option>
-        <option value="male">Male</option>
-        <option value="female">Female</option>
-        <option value="other">Other</option>
-=======
-          <form action="submit" className='form ' >
-            <div className='name'>
-              <h5>Name</h5>
-              <input type="text" placeholder="Enter Name" value={name} onChange={(e) => setName(e.target.value)} />
-              <h5>Gender</h5>
-              <select className='option' value={genderOptions} onClick={handleGender}>
-                <option value="male">Male</option>
-                <option value="female">Female</option>
-                <option value="other">Other</option>
->>>>>>> 85161aef2b8f7b2cd67111249775ed7cf03e8915
-              </select>
-            </div>
-            <div className='number'>
-              <h5>Age</h5>
-<<<<<<< HEAD
-              <input type="text" value={age} onChange={(e) => setAge(e.target.value)} placeholder="Enter Age" />
-              <h5>Phone No.</h5>
-              <input type="text" value={phone_no} onChange={(e) => setPhone(e.target.value)} placeholder="Enter Phone No." style={{ paddingBottom: "10px" }} />
-=======
-              <input type="text" placeholder="Enter Age" value={ age} onChange={(e) => setAge(e.target.value)}/>
-              <h5>Phone No.</h5>
-              <input type="text" placeholder="Enter Phone No." style={{ paddingBottom: "10px" }} value={phone} onChange={(e) => setPhone(e.target.value)}/>
->>>>>>> 85161aef2b8f7b2cd67111249775ed7cf03e8915
-            </div>
-          <div className='email'>
-            <h5>Email</h5>
-<<<<<<< HEAD
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter email" />
-=======
-            <input type="text" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)}/>
->>>>>>> 85161aef2b8f7b2cd67111249775ed7cf03e8915
-          </div>
-
-          {/* Address */}
-          <h5 style={{ paddingLeft: "40px", paddingTop: "50px", fontWeight: "bold" }}>Address</h5>
-
-            <div className='address-one'>
-              <h5>Street</h5>
-<<<<<<< HEAD
-              <input type="text" value={street} onChange={(e) => setStreet(e.target.value)} placeholder="Enter street name" />
-              <h5>Address</h5>
-              <input type="text" value={address}  onChange={(e) => setAddress(e.target.value)} placeholder="Enter street name" />
-              <h5>City</h5>
-              <input type="text" value={city} onChange={(e) => setCity(e.target.value)} placeholder="Enter city name" />
-            </div>
-            <div className='address-one'>
-              <h5>State</h5>
-              <input type="text" value={state} onChange={(e) => setState(e.target.value)} placeholder="Enter your state name" />
-              <h5>Country </h5>
-              <input type="text" value={country} onChange={(e) => setCountry(e.target.value)} placeholder="Enter country name" style={{ paddingBottom: "10px" }} />
-            </div>
-          <div className='address-two'>
-            <h5>Zip</h5>
-            <input type="text" value={zip} onChange={(e) => setZip(e.target.value)} placeholder="Enter zip code" />
-=======
-              <input type="text" placeholder="Enter street name" value={street} onChange={(e) => setStreet(e.target.value)}/>
-              <h5>City</h5>
-              <input type="text" placeholder="Enter city name" value={city} onChange={(e) =>setCity(e.target.value)}/>
-            </div>
-            <div className='address-one'>
-              <h5>State</h5>
-              <input type="text" placeholder="Enter your state name" value={state} onChange={(e) =>setState(e.target.value)}/>
-              <h5>Country </h5>
-              <input type="text" placeholder="Enter country name" style={{ paddingBottom: "10px" }} value={country} onChange={(e) => setCountry(e.target.value)}/>
-            </div>
+          <form onSubmit={handleSubmit}>
+            <input type="text" name="name" value={formData.name} onChange={handleChange} placeholder="Name" />
+            <input type="text" name="age" value={formData.age} onChange={handleChange} placeholder="Age" />
+            <select name="gender" value={formData.gender} onChange={handleChange}>
+              <option value="">Select gender</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="other">Other</option>
+            </select>
+            <input type="text" name="address" value={formData.address} onChange={handleChange} placeholder="Address" />
+            <input type="text" name="city" value={formData.city} onChange={handleChange} placeholder="City" />
+            <input type="text" name="phone_no" value={formData.phone_no} onChange={handleChange} placeholder="Phone Number" />
+            <input type="text" name="zip" value={formData.zip} onChange={handleChange} placeholder="Zip Code" />
+            <input type="text" name="state" value={formData.state} onChange={handleChange} placeholder="State" />
+            <input type="text" name="street" value={formData.street} onChange={handleChange} placeholder="Street" />
+            <input type="text" name="country" value={formData.country} onChange={handleChange} placeholder="Country" />
+            <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder="Email" />
+            <button type="submit">Submit</button>
           </form>
-          <div className='address-two'>
-            <h5>Zip</h5>
-            <input type="text" placeholder="Enter zip code" value={zip} onChange={(e) =>  setZip(e.target.value)}/>
->>>>>>> 85161aef2b8f7b2cd67111249775ed7cf03e8915
-          </div>
-          { /* Additional Notes */}
 
-          <h5 style={{ paddingLeft: "40px", paddingTop: "55px", fontWeight: "bold" }}>Additional Note</h5>
-          <div className='additional'>
-            <textarea  id="" cols="75" rows="100" style={{ resize: "none", border: "none", width: "650px", height: "50px" }}></textarea>
-          </div>
-          <div className='price'>
-            <MDBListGroup style={{ minWidth: '20rem', marginRight: "20px", marginLeft: "20px", paddingRight: "20px" }} light small>
-              <MDBListGroupItem style={{ minWidth: '20rem', marginRight: "20px", marginLeft: "20px", paddingRight: "20px" }} className="d-flex justify-content-between align-items-center">
-                <span style={{ paddingLeft: "20px" }}>Total Price</span>
-                <span >$100</span>
-              </MDBListGroupItem>
-              <MDBListGroupItem className="d-flex justify-content-between align-items-center" style={{ minWidth: '20rem', marginRight: "20px", marginLeft: "20px", paddingRight: "20px", marginTop: "2px" }}>
-                <span style={{ paddingLeft: "20px" }}>Subtotal</span>
-                <span>$80</span>
-              </MDBListGroupItem>
-            </MDBListGroup>
-          </div>
-          <div>
-<<<<<<< HEAD
-            <button className="btn btn-block" type="submit" style={{ width: "700px", marginTop: "10px", backgroundColor: "black", color: "white" }} onSubmit={handleSubmit}>
-            <button className="btn btn-block" type="submit" style={{ width: "700px", marginTop: "10px", backgroundColor: "black", color: "white" }} onSubmit={handleSubmit}>
-=======
-            <button className="btn btn-block" type="submit" style={{ width: "700px", marginTop: "10px", backgroundColor: "black", color: "white" }}>
->>>>>>> 431f51f569eef0e1b544c55b8f857b95fe09afab
-              Place Order
-            </button>
-          </div>
-          </form>
-       
         </div>
       </section>
     </div>
-    // <form onSubmit={handleSubmit}>
-    //   <input type="text" name="name" value={formData.name} onChange={handleChange} placeholder="Name" />
-    //   <input type="text" name="age" value={formData.age} onChange={handleChange} placeholder="Age" />
-    //   <select name="gender" value={formData.gender} onChange={handleChange}>
-    //     <option value="">Select gender</option>
-    //     <option value="male">Male</option>
-    //     <option value="female">Female</option>
-    //     <option value="other">Other</option>
-    //   </select>
-    //   <input type="text" name="address" value={formData.address} onChange={handleChange} placeholder="Address" />
-    //   <input type="text" name="city" value={formData.city} onChange={handleChange} placeholder="City" />
-    //   <input type="text" name="phone_no" value={formData.phone_no} onChange={handleChange} placeholder="Phone Number" />
-    //   <input type="text" name="zip" value={formData.zip} onChange={handleChange} placeholder="Zip Code" />
-    //   <input type="text" name="state" value={formData.state} onChange={handleChange} placeholder="State" />
-    //   <input type="text" name="street" value={formData.street} onChange={handleChange} placeholder="Street" />
-    //   <input type="text" name="country" value={formData.country} onChange={handleChange} placeholder="Country" />
-    //   <input type="email" name="email" value={formData.email} onChange={handleChange} placeholder="Email" />
-    //   <button type="submit">Submit</button>
-    // </form>
+
   );
 }
 
